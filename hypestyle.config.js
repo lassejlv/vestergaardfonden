655eb4159801b899d00d36c3b582e9e46c729105
lassev05 <vestergaardlasse2@gmail.com>
2022-06-17 12:51:52 +0200

    New changes!

--- hypestyle.config.js
+++ hypestyle.config.js
@@ -6,9 +6,13 @@ module.exports = {
     primary: "#4cae51",
     secondary: "#f5f5f5",
     heartRed: "#ef5261",
+    donate: "#19343f",
     stripe: "#6773e6",
+    stripeHover: "#5c67c9",
     mobilepay: "#5974f8",
+    mobilepayHover: "#4c6ac8",
     paypal: "#0293d3",
+    paypalHover: "#0387bf",
   },
 
   utils: {
--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -4,7 +4,27 @@ import Logo from "../logo.png";
 import { Link } from "react-router-dom";
 
 function Donate() {
-  const [valid, isValid] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const save = () => {
+    const email = document.getElementById("email").value;
+    const amount = document.getElementById("amount").value;
+    const creditCardNumber = document.getElementById("ccn").value;
+    const date = document.getElementById("date").value;
+    const cvc = document.getElementById("cvc").value;
+
+    const data = {
+      email,
+      amount,
+      creditCardNumber,
+      date,
+      cvc,
+    };
+
+    localStorage.setItem("donation", JSON.stringify(data));
+    setIsOpen(false);
+    alert("Tak for din donation! ❤️");
+  };
 
   return (
     <>
@@ -42,26 +62,118 @@ function Donate() {
 
             <Link
               className="btn bg-stripe text-secondary mt-2 ml-1"
-              to={`/donate/stripe`}
+              to={`/donate?token=${localStorage.getItem("token")}`}
+              onClick={() => setIsOpen(true)}
             >
               <i class="fa fa-credit-card"></i> Stripe
             </Link>
 
             <Link
               className="btn bg-mobilepay text-secondary mt-2 ml-1"
-              to={`/donate/mobilepay`}
+              to={`/donate?token=${localStorage.getItem("token")}`}
+              onClick={() => setIsOpen(true)}
             >
               <i class="fa fa-mobile"></i> Mobilepay
             </Link>
 
             <Link
               className="btn bg-paypal text-secondary mt-2 ml-1"
-              to={`/donate/paypal`}
+              to={`/donate?token=${localStorage.getItem("token")}`}
+              onClick={() => setIsOpen(true)}
             >
               <i class="fa-brands fa-paypal"></i> Paypal
             </Link>
           </div>
         </div>
+        {isOpen && (
+          <>
+            <form>
+              <div class="mb-1 form-group mt-2">
+                <label for="amount">Email addresse</label>
+                <input
+                  type="email"
+                  id="email"
+                  class="form-control form-invalid form-valid"
+                  placeholder="Skriv din email addresse"
+                  required
+                />
+              </div>
+
+              <div class="mb-1 form-group">
+                <label for="amount">Beløb</label>
+                <input
+                  type="number"
+                  class="form-control form-invalid form-valid"
+                  id="amount"
+                  minLength={1}
+                  placeholder="Beløb"
+                  required
+                />
+              </div>
+
+              <div class="mb-1 form-group">
+                <label for="ccn">Kredit Kort Nummer:</label>
+                <input
+                  id="ccn"
+                  type="tel"
+                  inputmode="numeric"
+                  pattern="[0-9\s]{13,19}"
+                  autocomplete="cc-number"
+                  class="mb-1 form-invalid form-valid"
+                  maxlength="19"
+                  placeholder="xxxx xxxx xxxx xxxx"
+                  required
+                ></input>
+
+                <p class="font-sm text-gray-light-1">
+                  Vi vil aldrig offenliggøre dit kortnummer på nogen måder.
+                </p>
+              </div>
+
+              <div class="mb-1 form-group">
+                <label for="cvc">Kortkode:</label>
+                <input
+                  id="cvc"
+                  type="tel"
+                  inputmode="numeric"
+                  pattern="[0-9\s]{3,4}"
+                  autocomplete="cc-cvc"
+                  class="mb-1 form-invalid form-valid"
+                  maxlength="3"
+                  placeholder="xxx"
+                  required
+                ></input>
+              </div>
+
+              <div class="form-group">
+                <label for="exp">Udløbsdato:</label>
+                <input
+                  id="exp"
+                  type="date"
+                  inputmode="numeric"
+                  pattern="[0-9\s]{4,5}"
+                  autocomplete="cc-exp"
+                  class="mb-1 form-invalid form-valid"
+                  maxlength="5"
+                  placeholder="mm/yy"
+                  required
+                ></input>
+              </div>
+
+              <p class="mb-2 text-gray font-sm">
+                Powered by{" "}
+                <a href="https://example.com" class="text-blue-light-2">
+                  Scammed.com
+                </a>
+              </p>
+
+              <button onClick={save} class="btn bg-donate text-white mb-2">
+                <i class="fa-solid fa-circle-arrow-right"></i> Gennemfør
+                bettaling
+              </button>
+            </form>
+          </>
+        )}
       </div>
     </>
   );
